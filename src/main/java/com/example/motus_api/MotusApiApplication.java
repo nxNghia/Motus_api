53c01b9247e589675aa99f31b5044fad5a4b2aee
nxNghia <nghia1421999@gmail.com>
2022-10-11 03:14:23 +0700

    init commit

---
+++ src/main/java/com/example/motus_api/MotusApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.motus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotusApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotusApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/motus_api/MotusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotusApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
